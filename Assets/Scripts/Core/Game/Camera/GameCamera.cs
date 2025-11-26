e9d09802d71d2e4c7c2159e84482ec3ad38751b0
vlad <vlandgor@gmail.com>
2025-11-25 21:47:54 -0500

    Added GameCamera

---
+++ Assets/Scripts/Core/Game/Camera/GameCamera.cs
@@ -0,0 +1,17 @@
+﻿using Core.Game.Board;
+using UnityEngine;
+
+namespace Core.Game.Camera
+{
+    public class GameCamera : MonoBehaviour
+    {
+        [SerializeField] private UnityEngine.Camera _camera;
+
+        public UnityEngine.Camera Camera => _camera;
+        
+        public void Initialize(IBoardData boardData)
+        {
+            _camera.orthographicSize = boardData.Size.x + 0.5f;
+        }
+    }
+}
